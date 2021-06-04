6e8cdb0ebe8ca86515abd6ead11f92d56bed1376
yudongyue <yudongyue@daqo.com>
2021-06-04 16:27:29 +0800

    Initial commit

---
+++ dubbo-demo-consumer/src/main/java/com/yue/DubboDemoConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.yue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: yudongyue
+ * @date: Created in 2021/6/4 16:22
+ */
+@SpringBootApplication
+public class DubboDemoConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboDemoConsumerApplication.class, args);
+    }
+}
